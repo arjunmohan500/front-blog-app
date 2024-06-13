5aa7d2e922567835cac22ea2bbd096b545f03b1a
arjunmohan500 <arjunmohan500@gmail.com>
2024-06-13 12:47:33 +0530

    created

--- app.js
+++ app.js
@@ -1,33 +1,55 @@
-const mongoose=require("mongoose")
-const express=require("express")
-const cors=require("cors")
-const bcrypt=require("bcryptjs")
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
+const bcrypt = require("bcryptjs")
 
 
-const {blogmodel}=require("./model/Blog")
-const app=express()
+const { blogmodel } = require("./model/Blog")
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 mongoose.connect("mongodb+srv://arjun:arjun2001@cluster0.jyq1ewu.mongodb.net/blogdb?retryWrites=true&w=majority&appName=Cluster0")
 
 
-const generateHashedPassword=async(password)=>{
-const salt =await  bcrypt.genSalt(10)
-return bcrypt.hash(password,salt)
+const generateHashedPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(password, salt)
 }
 
 
-app.post('/signup',async(req,res)=>{
-    let input=req.body
-    let hashedpassword=await generateHashedPassword(input.password)
+app.post('/signup', async (req, res) => {
+    let input = req.body
+    let hashedpassword = await generateHashedPassword(input.password)
     console.log(hashedpassword)
-    input.password=hashedpassword
-    let blog=new blogmodel(input)
+    input.password = hashedpassword
+    let blog = new blogmodel(input)
     blog.save()
-res.json({"status":"success"})
+    res.json({ "status": "success" })
 })
 
-app.listen(8080,()=>{
+app.post('/signin', (req, res) => {
+    let input = req.body
+    blogmodel.find({ "email": req.body.email }).then(
+        (response) => {
+            if (response.length > 0) {
+                let dbpassword = response[0].password
+                console.log(dbpassword)
+                bcrypt.compare(input.password, dbpassword, (error, ismatch)=> {
+                    if (ismatch) {
+                        res.json({ "status": "success", "userId": response[0]._id })
+
+                    } else {
+                        res.json({ "status": "incorrect" })
+                    }
+            })
+        }else {
+                res.json({ "status": "user not found" })
+            }
+        }
+    ).catch()
+})
+
+app.listen(8080, () => {
     console.log("server started")
 })
